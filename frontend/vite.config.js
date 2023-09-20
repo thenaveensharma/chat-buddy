46907f167f50b6109385021081243e36f6c57113
Naveen Kumar <naveenchaurasiya1501@gmail.com>
2023-09-20 23:56:59 +0530

    edit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      // "/api": "http://localhost:8080",
+      "/api": "http://localhost:8080",
       // "/api": "https://chat-buddy-fo2j.onrender.com", //production url
       // Add more proxy configurations if needed
     },
